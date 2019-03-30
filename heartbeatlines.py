831d13362b455fb3ef248ad9f7d046e70378c846
Fab <fabian@fabianhaupt.de>
2019-03-30 13:44:47 -0400

    hb modifications

--- heartbeatlines.py
+++ heartbeatlines.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[82]:
+# In[1]:
 
 
 from PIL import Image
@@ -12,11 +12,9 @@ from numpy import pi
 from numpy import sin
 WIDTH=1440
 HEIGHT=900
-# PIL accesses images in Cartesian co-ordinates, so it is Image[columns, rows]
 
 
-
-# In[78]:
+# In[65]:
 
 
 def splinei(it=30):
@@ -30,43 +28,42 @@ def splinei(it=30):
     yield curve_points
     for iter in range(it):
         for p in lpoints:
-            p[1]+=int((random()-0.5)*(60)*sin((p[0])*(pi/WIDTH)))
-            p[0]+=int((random()-0.5)*(20)*sin((p[0])*(pi/WIDTH)))
+            p[1]+=int((random()-0.5)*(40)*sin((p[0])*(pi/WIDTH))**2)
+            p[0]+=int((random()-0.5)*(10)*sin((p[0])*(pi/WIDTH))**2)
         curve.ctrlpts=lpoints
         curve_points=curve.evalpts
         yield curve_points
 
 
-# In[88]:
+# In[77]:
 
 
 def splinetest(pixels):
     splineit=splinei(500)
     decay=.91
     color=255
-    for s in range(100):
-        color*=decay
+    for s in range(300):
+        color=2#(s*2)+49
         p = next(splineit)
         for sp in p:
             x=int(sp[0])
             y=int(sp[1])
             if (x>=0) and (x<WIDTH) and (y>=0) and (y<HEIGHT):
                 c=pixels[x,y]
-                newc=tuple(map(sub,c,(int(color),int(color),int(color),1)))
+                #newc=tuple(map(sub,c,(int(color),int(color),int(color),1)))
+                newc=tuple(map(sub,c,(int(color),int(color),int(color))))+tuple([255])
                 #print(newc)
                 pixels[x,y]=newc
-                
-    
 
 
-# In[90]:
+# In[78]:
 
 
 img = Image.new( 'RGBA', (WIDTH,HEIGHT), "white") # create a new black image
 pixels = img.load() # create the pixel map
 seed()
 splinetest(pixels)
-img.show()
+img.save('hb.png')
 
 
 # In[ ]:
